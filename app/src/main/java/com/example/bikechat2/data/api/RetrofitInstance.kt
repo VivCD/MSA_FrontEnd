fee513517abf2a33395d43226c032b355d502215
VivCD <dumitrescucarla2@gmail.com>
2024-11-18 19:30:32 +0200

    FriendsScreen updated

--- app/src/main/java/com/example/bikechat2/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/bikechat2/data/api/RetrofitInstance.kt
@@ -1,4 +1,4 @@
-package com.example.bikechat2
+package com.example.bikechat2.data.api
 
 
 import retrofit2.Retrofit
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
     // Replace this with your ngrok URL
-    private const val BASE_URL = "https://2cbf-2a02-2f0b-5208-2c00-140d-250d-6971-7659.ngrok-free.app"
+    private const val BASE_URL = "https://2d3b-2a02-2f0b-5208-2c00-8832-643b-2114-3731.ngrok-free.app"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
